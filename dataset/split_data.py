acda88b881bbb8458350bbe48de323ca17d2af61
tabeer-asif <72459622+tabeer-asif@users.noreply.github.com>
2026-03-04 11:09:45 +0000

    Clean up split_data.py
    Removed print statements for displaying dataset shapes.

--- dataset/split_data.py
+++ dataset/split_data.py
@@ -3,10 +3,10 @@ from dont_patronize_me import DontPatronizeMe
 dpm = DontPatronizeMe('.', 'dontpatronizeme_pcl.tsv')
 # This method loads the subtask 1 data
 dpm.load_task1()
-# which we can then access as a dataframe
+
 df = dpm.train_task1_df
 
-# Load the dev and train set par_id lists
+
 dev_par_ids = pd.read_csv("dev_semeval_parids-labels.csv")["par_id"]
 train_par_ids = pd.read_csv("train_semeval_parids-labels.csv")["par_id"]
 
@@ -15,10 +15,5 @@ df_par_ids = df["par_id"].astype("Int64")
 df_dev = df[df_par_ids.isin(dev_par_ids)].dropna()
 df_train = df[df_par_ids.isin(train_par_ids)].dropna()
 
-# Display results
-print("Dev Set:", df_dev.shape)
-print("Train Set:", df_train.shape)
-
-# Save if needed
 df_dev.to_csv("dev_set.csv", index=False)
 df_train.to_csv("train_set.csv", index=False)
